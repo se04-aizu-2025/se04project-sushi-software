a58891330a8edbd0542017174158e2179ab6a10a
fjm2u <141835061+fjm2u@users.noreply.github.com>
2026-01-23 16:52:07 +0900

    Add InsertionSort and QuickSort to sorter module; implement random data generator (#8)

--- sorters/__init__.py
+++ sorters/__init__.py
@@ -3,7 +3,9 @@
 from .base import Sorter
 from .bubble import BubbleSort
 from .heap import HeapSort
+from .insertion import InsertionSort
 from .merge import MergeSort
+from .quick import QuickSort
 from .selection import SelectionSort
 
-__all__ = ["Sorter", "BubbleSort", "HeapSort", "MergeSort", "SelectionSort"]
+__all__ = ["Sorter", "BubbleSort", "HeapSort", "InsertionSort", "MergeSort", "QuickSort", "SelectionSort"]
